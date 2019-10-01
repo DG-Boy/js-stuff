06daad82e8b45474cfc5ddc022f6b79d749d09f2
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2019-10-01 19:22:38 +0300

    Третий чек

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var ctrlMain = require('../controllers/main')
 var router = express.Router();
 
+const bodyParser = require("body-parser");
+const parser = bodyParser.urlencoded({extended: false});
+
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
@@ -9,9 +12,13 @@ router.get('/image', function(req, res) {
     res.sendFile(__dirname + "/ambal.png");
 });
 
-router.get('/gimme', function(req, res) {
-    res.sendFile(__dirname + "/rezume.html");
+
+router.get('/css', parser, function(req, res) {
     res.sendFile(__dirname + "/style.css");
+});
+
+router.get('/gimme/html', parser, function(req, res) {
+    res.sendFile(__dirname + "/rezume.html");
 });
 
 module.exports = router;

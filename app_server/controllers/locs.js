51210b76968a4e00f4c8e1a69fb90925b44bcbc5
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2019-10-04 18:19:40 +0300

    Вынесены данные для homepage из views в ctrls

--- app_server/controllers/locs.js
+++ app_server/controllers/locs.js
@@ -1,6 +1,32 @@
 /* Получаем (GET) домашнюю страницу */
 module.exports.homelist = function (req, res) {
-  res.render('locs-list', { title: 'Главная' });
+  res.render('locs-list', { 
+    title: 'Loc8r - всегда покажет, что находится рядом ;)',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'всегда покажет, что находится рядом ;)'
+
+    },
+    locs: [{
+      name: 'Общага',
+      address: 'ул. Студенческая, д.6/б',
+      rating: '5',
+      facilities: ['Вечно нет горячей воды ', 'Лидка', 'Обрыганная кухня every day'],
+      distance: '0м'
+    }, {
+      name: 'НРТК',
+      address: 'ул. Студенческая, д.6',
+      rating: '4',
+      facilities: ['Дудос', 'Токарь', 'Чёпа', 'Вадик', 'Дмитра', 'Поц'],
+      distance: '10м'
+    }, {
+      name: 'Пятёрочка',
+      address: 'пр. Гагарина',
+      rating: '3',
+      facilities: ['Где сырки!', 'Красная цена'],
+      distance: '100м'
+    }]
+  });
 };
 
 /* Получаем (GET) страницу с информацией о локации */

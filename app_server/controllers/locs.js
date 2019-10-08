9ee8882f6ff26ffc57da1b14e095a9d556794893
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2019-10-08 17:28:04 +0300

    Обновлены все оставшиеся представления

--- app_server/controllers/locs.js
+++ app_server/controllers/locs.js
@@ -5,8 +5,8 @@ module.exports.homelist = function (req, res) {
     pageHeader: {
       title: 'Loc8r',
       strapline: 'всегда покажет, что находится рядом ;)'
-
     },
+    sidebar: "Ехал Грека через реку",
     locs: [{
       name: 'Общага',
       address: 'ул. Студенческая, д.6/б',
@@ -31,10 +31,57 @@ module.exports.homelist = function (req, res) {
 
 /* Получаем (GET) страницу с информацией о локации */
 module.exports.locInfo = function (req, res) {
-  res.render('loc-info', { title: 'Информация о местоположении' });
+  res.render('loc-info', { 
+    title: 'Общага',
+    pageHeader: {title: 'Общага'},
+    sidebar: {
+      context: 'Sugar? Yeaah, gimme some, gimme dome, i\'ma smoke some',
+      callToAction: 'Let\'s get it!'
+    },
+    loc: {
+      name: 'Общага',
+      address: 'ул.Студентческая, д.6/б',
+      rating: 5,
+      facilities: ['Вечно нет горячей воды ', 'Лидка', 'Обрыганная кухня every day'],
+      coords: {lat: 43.987239, lng: 56.302408},  
+      openingTimes: [{
+        days: 'Пн - Пт',
+        opening: '7:00',
+        closing: '21:00',
+        closed: false
+      }, {
+        days: 'Сб',
+        opening: '8:00',
+        closing: '17:00',
+        closed: false
+      }, {
+        days: 'Вс',
+        closed: true
+      }],
+      reviews: [{
+        author: 'DG Boy',
+        rating: 5,
+        timestamp: '08.10.2019',
+        reviewText: 'Wow!'
+      }, {
+        author: 'Стики Фингер',
+        rating: 4,
+        timestamp: '03.10.2019',
+        reviewText: 'Только finger щекочет мой ass!!!'
+      }, {
+        author: 'Егорка Ебоиийка',
+        rating: 1,
+        timestamp: '02.09.2019',
+        reviewText: 'Непросто быть собой, когда шагает boy'
+      }]
+    }
+  });
 };
 
 /* Получаем (GET) страницу с для добавления отзыва */
 module.exports.addReview = function (req, res) {
-  res.render('add-review', { title: 'Добавление отзыва' });
+  res.render('add-review', { 
+    title: 'Добавление отзыва на Loc8r',
+    pageHeader: {title: 'Добавление отзыва'},
+  });
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* Получаем (GET) страницу "О нас" */
 module.exports.about = function (req, res) {
-    res.render('general-text', { title: 'О нас' });
+    res.render('general-text', { 
+      title: 'О нашем Loc8r',
+      content: 'Loc8r создан для помощи людям, ищущих помощь. \nТра-ля-ля, тру-лю-люм. YEAH!'
+    });
   };

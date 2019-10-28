6bf5d56296add43fab36411b5894c5aca4f98f76
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2019-10-28 23:11:45 +0300

    Завершение работы над API: реализовано 9 запросов

--- app_api/controllers/locs.js
+++ app_api/controllers/locs.js
@@ -1,41 +1,222 @@
 var mongoose = require('mongoose');
-var loc = mongoose.model('loc');
+var Loc = mongoose.model('loc');
 
 var sendJsonRes = function(res, status, content) {
-    res.status(status);
-    res.json(content);
-}
+	res.status(status);
+	res.json(content);
+};
+
+var theEarth = (function() {
+	var earthRadius = 6371;
+	
+	var getDistanceFromRads = function(rads) {
+		return parseFloat(rads * earthRadius);
+	};
+	var getRadsFromDistance = function(distance) {
+		return parseFloat(distance / earthRadius);
+	};
+	
+	return {
+		getDistanceFromRads : getDistanceFromRads,
+		getRadsFromDistance : getRadsFromDistance
+	};
+})();
 
 module.exports.locsListByDistance = function (req, res) {
-    sendJsonRes(res, 200, {"status": "success"});
+	var lng = parseFloat(req.query.lng);
+	var lat = parseFloat(req.query.lat);
+	//var maxDistance = parseFloat(req.query.maxdist);
+	var point = {
+		type: "Point",
+		coordinates: [lng, lat]
+	};
+	
+	var geoOptions = {
+		spherical: true,
+		maxDistance: theEarth.getRadsFromDistance(req.query.maxdist),
+		num: 10
+	};
+	
+	if(!lng || !lat || !req.query.maxdist) {
+		sendJsonRes(res, 404, {
+			"message": "lng and lat query parameters are required"
+		});
+		return;
+	}
+
+	Loc.geoNear(point, geoOptions, function (err, results, stats) {
+		//console.log(results);
+		var locations = [];
+		if(err) {
+			sendJsonRes(res, 404, err);
+		} else {
+			results.forEach(function(doc) {
+				locations.push({
+					distance: theEarth.getRadsFromDistance(doc.dis),
+					name: doc.name,
+					address: doc.address,
+					rating: doc.rating,
+					facilities: doc.facilities,
+					_id: doc._id
+				});
+			});
+			sendJsonRes(res, 200, locs);
+		}
+	});
+/*
+	Loc.aggregate(
+		[{
+			$geoNear: {
+				near: { type: "Point", coordinates: [ lng, lat ] },
+				spherical: true,
+				distanceField: "dist.calculated",
+				includeLocs: "dist.location",
+				maxDistance: maxDistance
+			}
+		}], 
+		function(err, results) {
+			console.log(results)
+			if(err) {
+				sendJsonRes(res, 404, err);
+			} else {
+				var locs = [];
+				results.forEach(function(doc) {
+					locs.push({
+						distance: doc.dist,
+						name: doc.name,
+						address: doc.address,
+						rating: doc.rating,
+						facilities: doc.facilities,
+						_id: doc._id
+					});
+				});
+				sendJsonRes(res, 200, locs);
+			}
+		}
+	);*/
 };
 module.exports.locsCreate = function (req, res) {
-    sendJsonRes(res, 200, {"status": "success"});
+	Loc.create({
+		name: req.body.name,
+		address: req.body.address,
+		facilities: req.body.facilities.split(","),
+		coords: [
+			parseFloat(req.body.lng),
+			parseFloat(req.body.lat)
+		],
+		openingTimes: [
+		{
+			days: req.body.days1,
+			opening: req.body.opening1,
+			closing: req.body.closing1,
+			closed: req.body.closed1
+		}, {
+			days: req.body.days2,
+			opening: req.body.opening2,
+			closing: req.body.closing2,
+			closed: req.body.closed2
+		}]
+	}, function(err, loc) {
+		if(err) {
+			sendJsonRes(res, 400, err);
+		} else {
+			sendJsonRes(res, 201, loc);
+		}
+	}
+	)
 };
 module.exports.locsReadOne = function (req, res) {
-    if(req.params && req.params.locid) {
-        loc.findById(req.params.locid).exec(function(err, loc) {
-            if(!loc) {
-                sendJsonRes(res, 404, {
-                    "message": "fuck you!"
-                });
-                return;
-            } else if(err) {
-                sendJsonRes(res, 404, err);
-                return;
-            }
+	if(req.params && req.params.locid) {
+		Loc.findById(req.params.locid).exec(function(err, loc) {
+			if(!loc) {
+				sendJsonRes(res, 404, {
+					"message": "Where is my loc?!"
+				});
+				return;
+			} else if(err) {
+				sendJsonRes(res, 404, err);
+				return;
+			}
 
-            sendJsonRes(res, 200, loc);
-        });
-    } else {
-        sendJsonRes(res, 404, {
-            "message": "No locid in request!"
-        });
-    }
+			sendJsonRes(res, 200, loc);
+		});
+	} else {
+		sendJsonRes(res, 404, {
+			"message": "No locid in request!"
+		});
+	}
 };
 module.exports.locsUpdateOne = function (req, res) {
-    sendJsonRes(res, 200, {"status": "success"});
+	if(!req.params.locid) {
+		sendJsonRes(res, 404, {
+			"message": "No found, locID is required!"
+		});
+		return;
+	} else {
+		// 1. Найти соответсвующий документ
+		Loc
+		.findById(req.params.locid)
+		.select('-reviews -rating')
+		.exec(
+			function(err, loc) {
+				if(!loc) {
+					sendJsonRes(res, 404, {
+						"message": "locID not found!"
+					});
+					return;
+				} else if(err) {
+					sendJsonRes(res, 400, err);
+					return;
+				}
+
+				// 2. Выполнить какие-либо изменения экземпляра
+				loc.name = req.body.name;
+				loc.address = req.body.address;
+				loc.facilities = req.body.facilities.split(",");
+				loc.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+				loc.openingTimes = [{
+						days: req.body.days1,
+						opening: req.body.opening1,
+						closing: req.body.closing1,
+						closed: req.body.closed1
+					}, {
+						days: req.body.days2,
+						opening: req.body.opening2,
+						closing: req.body.closing2,
+						closed: req.body.closed2
+					}];
+
+				// 3. Сохранить документ
+				loc.save(function(err, loc) {
+					// 4. Отправить JSON-ответ
+					if(err) {
+						sendJsonRes(res, 404, err);
+					} else {
+						sendJsonRes(res, 200, loc);
+					}
+				});
+		});
+	}
 };
 module.exports.locsDeleteOne = function (req, res) {
-    sendJsonRes(res, 200, {"status": "success"});
+	var locid = req.params.locid;
+	if(locid) {
+		Loc
+		.findByIdAndRemove(locid)
+		.exec(
+			function(err, loc) {
+				if(err) {
+					sendJsonRes(res, 404, err);
+					return;
+				}
+				
+				// Loc.remove(function(err, loc) {}); 
+
+				sendJsonRes(res, 204, null);
+		});
+	} else {
+		sendJsonRes(res, 404, {
+			"message": "No locID"
+		});
+	}
 };
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,17 +1,89 @@
 var mongoose = require('mongoose');
-var loc = mongoose.model('loc');
+var Loc = mongoose.model('loc');
 
 var sendJsonRes = function(res, status, content) {
 	res.status(status);
 	res.json(content);
 }
+var addReview = function(req, res, loc) {
+	if(!loc) {
+		sendJsonRes(res, 404, {
+			"message" : "locID not found"
+		});
+	} else {
+		loc.reviews.push({
+			author: req.body.author,
+			rating: req.body.rating,
+			reviewText: req.body.reviewText
+		});
+
+		loc.save(function(err, loc) {
+			var thisReview;
+			if(err) {
+				sendJsonRes(res, 400, err);
+			} else {
+				updateAverageRating(loc._id);
+				thisReview = loc.review[loc.review.length - 1];
+				sendJsonRes(res, 201, thisReview);
+			}
+		});
+	}
+};
+var updateAverageRating = function(locid) {
+	Loc
+	.findById(locid)
+	.select('rating reviews')
+	.exec(
+		function (err, locid) {
+			if(!err) {
+				setAverageRating(loc);
+			}
+		}
+	);
+};
+var setAverageRating = function (loc) {
+	var reviewCount, ratingAverage, ratingTotal;
+	if(loc.review && loc.reviews.length > 0) {
+		reviewCount = loc.review.length;
+		ratingTotal = 0;
+
+		for(var i = 0; i < reviewCount; i++) {
+			ratingTotal += loc.review[i].rating;
+		}
+		ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+		loc.rating = ratingAverage;
+		loc.save(function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log("Average rating updated to ", ratingAverage0);
+			}
+		});
+	}
+};
 
 module.exports.reviewsCreate = function (req, res) {
-	sendJsonRes(res, 200, {"status": "success"});
+	var locid = req.params.locid;
+	if(locid) {
+		Loc
+		.findById(locid)
+		.select('reviews')
+		.exec(function(err, loc) {
+			if(err) {
+				sendJsonRes(res, 400, err);
+			} else {
+				addReview(req, res, loc);
+			}
+		});
+	} else {
+		sendJsonRes(res, 404, {
+			"message": "No found, locID required!"
+		});
+	}
 };
 module.exports.reviewsReadOne = function (req, res) {
 	if(req.params && req.params.locid && req.params.reviewid) {
-		loc
+		Loc
 		.findById(req.params.locid)
 		.select('name reviews')
 		.exec(function(err, loc) {
@@ -55,8 +127,98 @@ module.exports.reviewsReadOne = function (req, res) {
 	}
 };
 module.exports.reviewsUpdateOne = function (req, res) {
-	sendJsonRes(res, 200, {"status": "success"});
+	if(!req.params.locid || !req.params.reviewid) {
+		sendJsonRes(res, 404, {
+			"message": "No found, locID and reviewID is both required!"
+		});
+		return;
+	}
+
+	//1. Найти соответсвующий документ
+	//2. Найти соответсвующий поддокумент
+	Loc
+	.findById(req.params.locid)
+	.select('reviews')
+	.exec(
+		function(err, loc) {
+			var thisReview;
+			if(!loc) {
+				sendJsonRes(res, 404, {
+					"message": "locID not found!"
+				});
+				return;
+			} else if(err) {
+				sendJsonRes(res, 400, err);
+				return;
+			}
+
+			if(loc.reviews && loc.reviews.length > 0) {
+				thisReview = loc.reviews.id(req.params.reviewid);
+				if(!thisReview) {
+					sendJsonRes(res, 404, {
+						"message": "reviewid not found"
+					});
+				} else {
+					//3. Выполнить какие-либо изменения экземпляра
+					thisReview.author = req.body.author;
+					thisReview.rating = req.body.rating;
+					thisReview.reviewText = req.body.reviewText;
+
+					//4. Сохранить документ
+					loc.save(function(err, loc) {
+						//5. Отправить JSON-ответ
+						if(err) {
+							sendJsonRes(res, 404, err);
+						} else {
+							updateAverageRating(loc._id);
+							sendJsonRes(res, 200, thisReview);
+						}
+					});
+				}
+			} else {
+				sendJsonRes(res, 404, {
+					"message": "No review to update"
+				});
+			}
+	});
 };
 module.exports.reviewsDeleteOne = function (req, res) {
-	sendJsonRes(res, 200, {"status": "success"});
+	if(!req.params.locid || !req.params.reviewid) {
+		sendJsonRes(res, 404, {
+			"message": "No found, locID and reviewID is both required!"
+		});
+		return;
+	}
+
+	Loc
+	.findById(req.params.locid)
+	.select('reviews')
+	.exec(
+		function(err, loc) {
+			if(!loc) {
+				sendJsonRes(res, 404, {"message": "locID not found!"});
+				return;
+			} else if(err) {
+				sendJsonRes(res, 400, err);
+				return;
+			}
+
+			if(loc.reviews && loc.reviews.length > 0) {
+				if(!loc.reviews.id(req.params.reviewid)) {
+					sendJsonRes(res, 404, {"message": "reviewid not found"});
+				} else {
+					loc.reviews.id(req.params.reviewid).remove();
+					loc.save(function(err) {
+						if(err) {
+							sendJsonRes(res, 404, err);
+						} else {
+							updateAverageRating(loc._id);
+							sendJsonRes(res, 204, null);
+						}
+					});
+				}
+			} else {
+				sendJsonRes(res, 404, {"message": "No review to update"});
+			}
+	});
 };
